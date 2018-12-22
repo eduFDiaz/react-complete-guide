e06cc4ad3d79833b68ea4eed1eeae119ffe7deac
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2018-12-21 21:30:33 -0600

    working with states is the next section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React developer</h1>
+        <h1>Hi, I'm a React developer, my name is Eduardo</h1>
+        <p>This is really working!</p>
+        <Person name="Edu" age="32"/>
+        <Person name="Yani" age="17"> My hobbies: Racing</Person>
+        <Person name="Consuelo" age="50"/>
       </div>
     );
   }
