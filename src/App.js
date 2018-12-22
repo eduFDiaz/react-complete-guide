42a76c8b9299c2bfb3ce91d1ea5ce9ef78481cd2
Eduardo A Fernandez Diaz <fernandez9000@gmail.com>
2018-12-22 10:03:12 -0600

    two way binding

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ class App extends Component {
       }
     )
   }
+  nameChangeHandler = (event) => {
+    this.setState({
+        persons: [
+          {name: "Eduardo", age: 32},
+          {name: event.target.value, age: 17},
+          {name: "Consuelo", age: 50}
+        ]
+      }
+    )
+  }
   render() {
     return (
       <div className="App">
@@ -34,7 +44,8 @@ class App extends Component {
           click={this.switchNameHandler} 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this,"Eduardito")}> My hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this,"Eduardito")}
+          changed={this.nameChangeHandler.bind(this)}> My hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,6 +8,7 @@ class Person extends Component{
                 <h1 onClick={this.props.click} >{this.props.name}</h1>
                 <p>Age: {this.props.age}</p>
                 <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
             </div>
         );
     }
