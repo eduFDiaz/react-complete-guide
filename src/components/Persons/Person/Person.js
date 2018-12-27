1bd42f431369c381e1932d3fc747328eafc9bf38
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2018-12-27 14:37:14 -0600

    102 another way of using references

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,7 @@ class Person extends Component{
     constructor(props){
         super(props);
         console.log('[Person.js] Inside constructor()',props);
+        this.inputElement = React.createRef();
       }
       componentWillMount(){
         console.log('[Person.js] Inside componentWillMount()');
@@ -14,7 +15,7 @@ class Person extends Component{
       componentDidMount(){
         console.log('[Person.js] Inside componentDidMount()');
         if (this.props.position === 0){
-            this.inputElement.focus();
+            this.inputElement.current.focus();
         }
       }
     render () {
@@ -24,7 +25,7 @@ class Person extends Component{
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref={(inp) => { this.inputElement = inp }}
+                    ref={this.inputElement}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}></input>
