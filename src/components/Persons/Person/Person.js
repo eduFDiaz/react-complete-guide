dd85ebb964e04c8af8159ef8bc2ec3956d50cdec
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2018-12-27 14:26:29 -0600

    101 Using references

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -13,6 +13,9 @@ class Person extends Component{
     
       componentDidMount(){
         console.log('[Person.js] Inside componentDidMount()');
+        if (this.props.position === 0){
+            this.inputElement.focus();
+        }
       }
     render () {
         console.log('[Person.js] Inside render()');
@@ -20,7 +23,11 @@ class Person extends Component{
             <div key={this.props.id} className={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+                <input 
+                    ref={(inp) => { this.inputElement = inp }}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}></input>
             </div>
         );
     }
